1ff50d8b2133721b3376026e19d233e815620c06
suraj.be <suraj.be@media.net>
2022-11-08 10:50:13 +0530

    First C++ Program

---
+++ 001-helloworld.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  cout<<"Soon to become a software engineer!";
+  return 0;
+}
+
+/* 
+  #include<iostream> -> iostream is a header file, that includes many methods
+
+  using namespace std -> cout resides in this namespace, if this particular namespace is not used, then we'll have to write something like this: std::cout
+*/
