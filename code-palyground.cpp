018cb010be29e61f4af0c3e6779d6456673179ae
suraj.be <suraj.be@media.net>
2022-12-07 16:35:41 +0530

    folder restructure

---
+++ code-palyground.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int arr[5] = {1, 2, 3, 4, 5};
+  /*
+  printf("%d", 2[arr]);
+  printf("%d", arr[2]);
+  printf("%d", *(arr + 2));
+  --- All 3 of them are valid
+  */
+
+  return 0;
+}
